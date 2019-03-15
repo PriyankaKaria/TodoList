c91bba564a4d1b3ed567f4a73dca6d8379969120
PriyankaKaria <48031076+PriyankaKaria@users.noreply.github.com>
2019-03-15 13:10:51 +0100

    User Class

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -8,7 +8,7 @@ public class User {
     private Tasker tasker;
     private String login;
     private String password;
-
+//login and password
     public User(String login, String password) {
         this.login = login;
         this.password = password;
